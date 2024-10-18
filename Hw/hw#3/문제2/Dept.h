547306d132e9f1125571db6ce4a89bc396954ac5
dbstjq <94620310+dbstjq@users.noreply.github.com>
2024-10-18 18:14:59 +0900

    Create Dept.h

---
+++ Hw/hw#3/문제2/Dept.h
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+class Dept {
+    int size;
+    int* scores;
+public:
+    Dept(int size) {
+        this->size = size;
+        scores = new int[size];
+    }
+    ~Dept();
+    int getSize() { return size; }
+    void read();
+    bool isOver60(int index);
+};
